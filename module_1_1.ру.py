41e788c4e5bfa7a8741b89a8e6addba25e3f7ca8
Samvel <tradeup.education@gmail.com>
2024-07-30 15:17:01 +0300

    Add files via upload

---
+++ module_1_1.ру.py
@@ -0,0 +1,6 @@
+example = "Urban"
+print(example [0:1])       # Ответ: U
+print(example[-1])         # Ответ: n
+print(example[2:5])        # Ответ: ban
+print(example[4::-1])      # Ответ:nabrU
+print(example[1:5:2])      # Ответ:ra
